982e262e4584f9a44838ef550efbb36979435a21
Hammedat <159439037+Hammedat@users.noreply.github.com>
2024-02-08 04:28:50 -0800

    Add files via upload

---
+++ AMMIE.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $baba = 7;
+        $i = 0;
+        $da = 4.9;
+
+        
+        $d = 0.0;
+        $s = "AMIDAT";
+
+        echo $baba + $i . "<br>";
+
+        echo $da + $d ."<br>";
+
+        echo "Your name is ". $str;
+
+
+
+
+
+    ?>
+</body>
+</html>
